dd46c9c1b00df550d794d76fe24c70e61585722f
MarkoBojanic18 <markobojanic18@gmail.com>
2024-01-02 17:51:12 +0100

    Napravljen stream_processor koji sluzi kao posrednik izmedju producer-a i consumer-a

---
+++ stream_processor.py
@@ -0,0 +1,34 @@
+from kafka import KafkaConsumer, KafkaProducer
+import requests, json
+
+#configuration
+bootstrap_servers = 'localhost:9092'
+input_topic = 'forcast_raw_weather'
+output_topic = 'forcast_prep_weather'
+
+#consumer
+consumer = KafkaConsumer(input_topic, bootstrap_servers=bootstrap_servers,value_deserializer=lambda x:json.loads(x.decode('utf-8')))
+
+#producer
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers,value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
+
+for message in consumer:
+    #take json file
+    json_data = message.value
+
+    temperature_kelvin = float(json_data['main']['temp'])
+    temperature_celsius = temperature_kelvin - 273.15
+    print(f"Temperature in Kelvin: {temperature_kelvin} //// Temperature in Celsius: {temperature_celsius}")
+
+    processed_data = {
+        'city': json_data.get('name','Unknown City'),
+        'temperature_celsius':round(temperature_celsius,2),
+        'dt': json_data.get('dt','error no date')
+    }
+
+    producer.send(output_topic, value=processed_data)
+    producer.flush()
+
+consumer.close()
+producer.close()
